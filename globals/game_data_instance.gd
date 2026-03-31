23f615de0a6af66f5ab125e20218f2a5dfacea12
guigs <ohmyguigs@gmail.com>
2026-03-31 02:28:11 -0300

    v0.0.2 - save data and edit name

---
+++ globals/game_data_instance.gd
@@ -0,0 +1,25 @@
+class_name GameDataInstance
+extends Node
+
+const SAVE_GAME_PATH := "user://save_game_data.tres"
+const GUILDS: Array[String] = ["red", "blue", "black", "purple", "yellow"]
+const ROLES: Array[String] = ["warrior", "lancer", "monk", "archer"]
+
+var local_game_save: GameData = null
+
+func _ready() -> void:
+	if ResourceLoader.exists(SAVE_GAME_PATH):
+		local_game_save = ResourceLoader.load(SAVE_GAME_PATH, "", ResourceLoader.CACHE_MODE_IGNORE)
+	else:
+		write_gamesave(GameData.new())
+
+func write_gamesave(game_data: GameData) -> void:
+	var error_code := ResourceSaver.save(game_data, SAVE_GAME_PATH)
+	if error_code != OK:
+		push_error("Failed to save game: " + error_string(error_code))
+
+func load_gamesave(_device_id: String) -> GameData:
+	if ResourceLoader.exists(SAVE_GAME_PATH):
+		local_game_save = ResourceLoader.load(SAVE_GAME_PATH, "", ResourceLoader.CACHE_MODE_IGNORE)
+	# use _device_id to fetch save from server instead
+	return local_game_save
---
+++ resources/game_data.gd
@@ -0,0 +1,9 @@
+class_name GameData
+extends Resource
+
+# initial global game data
+@export var display_name:String = "newb_1337"
+@export var device_id:String = "desktop_newb_1337"
+@export var selected_guild:String = "red"
+@export var selected_role:String = "warrior"
+@export var player_global_position := Vector2(0, 0)
